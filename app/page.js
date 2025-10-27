e655b6816d1a2909dfe020a984a3e0a7fb3e439d
huzaifawoltrio <huzaifa@woltrio.com>
2025-10-27 13:39:16 +0500

    first commit

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import Home8 from "./(homes)/home-8/page";
+export const metadata = {
+  title:
+    "Home 1 || Lexend - Full-featured, professional-looking software, saas and startup nextjs template.",
+  description:
+    "Lexend - Full-featured, professional-looking software, saas and startup nextjs template.",
+};
+export default function HomePage1() {
+  return (
+    <>
+      <Home8 />
+    </>
+  );
+}
